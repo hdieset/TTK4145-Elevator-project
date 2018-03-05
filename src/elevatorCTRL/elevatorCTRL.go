85a628c8dc774e0e3797e7135ead2c3fc4d1c3ff
molvik <oystein.m@hotmail.com>
2018-03-05 20:35:50 +0100

    Glemte å pushe denne.

--- src/elevatorCTRL/elevatorCTRL.go
+++ src/elevatorCTRL/elevatorCTRL.go
@@ -14,8 +14,8 @@ func main(){
     fmt.Println(dir,"\n")
 	cmd := exec.Command("sh", "-c", dir+"/hall_request_assigner", "-i", `'{"hallRequests":[[false,false],[true,false],[false,false],[false,true]],"states":{"one":{"behaviour":"moving","floor":2,"direction":"up","cabRequests":[false,false,true,true]},"two":{"behaviour":"idle","floor":0,"direction":"stop","cabRequests":[false,false,false,false]}}}'`)
 	fmt.Println(cmd, "\n")	
-	result, err:= cmd.Output()
-
+	result, err:= cmd.Output(
+)
 	fmt.Println("\nhall request assigner result:", result, err)
 
 
