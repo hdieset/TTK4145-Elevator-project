8595ab5c2c293a9e582f1df05217cc797b5b061a
Herman Dieset <hdieset@gmail.com>
2018-03-09 11:27:16 +0100

    UPD8

--- src/SingleElevator/requests.go
+++ src/SingleElevator/requests.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 const ( // Some constans that should not be decleared here.
-	N_FLOORS = 3
-	N_BUTTONS = 4
+	N_FLOORS = 4
+	N_BUTTONS = 2
 	DoorOpenDuration_s = 5
 )
 
@@ -33,14 +33,46 @@ type Elevator struct {
 	}
 }
 
-/*func requests_above(x int) int {
-	
-}*/
+func requests_above(e Elevator) bool {
+	for f := e.floor + 1; f < N_FLOORS; f++ {
+		for btn := 0; btn < N_BUTTONS; btn++ {
+			if e.requests[f][btn] == 1 {
+				return true
+			}
+		}
+	} 
+	return false
+}
+
+func requests_below(e Elevator) bool {
+	for f := 0; f < e.floor; f++ {
+		for btn := 0; btn < N_BUTTONS; btn++ {
+			if e.requests[f][btn] == 1 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func requests_chooseDirection(e Elevator) Dirn {
+	switch e.dirn {
+	case D_Up :
+		if requests_above(e) { return D_Up }
+		else if requests_below(e) {return D_Down}
+		else {return D_Stop}
+	}
+}
 
 func main(){
+	//p() := fmt.Println()
 	var heis Elevator
-	heis.config.doorOpenDuration_s = DoorOpenDuration_s
+	//heis.config.doorOpenDuration_s = DoorOpenDuration_s
 	heis.floor = 2
+	heis.dirn = D_Up
 	fmt.Println(heis.floor)
-	fmt.Println(heis.config.doorOpenDuration_s)
+	heis.requests[1][1] = 1
+	fmt.Println(requests_below(heis))
+	fmt.Println(requests_chooseDirection(heis))
+	//p(heis.floor)
 }
